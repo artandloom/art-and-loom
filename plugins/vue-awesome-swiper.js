cae034ecba25b900ae68a25315836e779636cc9f
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-08-02 20:38:19 +0200

    Updating vue-swiper implementation

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,4 +1,14 @@
-import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import Vue from 'vue'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-Vue.use(VueAwesomeSwiper);
+// Vue.use(VueAwesomeSwiper);
+
+import Vue from 'vue'
+import { Swiper as SwiperClass, Pagination, Navigation } from 'swiper/swiper.esm'
+import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter'
+
+// import style
+// import 'swiper/swiper-bundle.min.css'
+
+SwiperClass.use([Pagination, Navigation])
+Vue.use(getAwesomeSwiper(SwiperClass))
