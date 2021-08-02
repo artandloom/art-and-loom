ebc407bb832aaca75be61920a7375a280219cca1
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-08-02 11:38:29 +0200

    Add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>
-    <div>
-      <div>Logo</div>
-      <div>redes</div>
-    </div>
-    <div>
-        navbar
-    </div>
-    <div>
-        copyright
-    </div>
-  </div>
+  <footer>
+    <section class="flex flex-row items-end justify-between pt-11 px-15">
+      <div>
+        <img src="/images/logo.svg" alt="Art and loom" />
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/artandloom/"
+          target="_blank"
+          rel="noopener"
+        >
+          <img src="/icons/instagram.svg" alt="Instagram" />
+        </a>
+      </div>
+    </section>
+    <section class="flex flex-row items-center justify-end mt-8 px-15">
+      <div class="text-right">
+        <p class="flex flex-row items-center">
+          <span class="flex mr-6 separator"></span>
+          For Interior Design
+        </p>
+        <p>igworkshop.com</p>
+      </div>
+    </section>
+    <nav class="m-auto w-3/4 my-28">
+      <ul class="flex flex-row list-none m-0 p-0 justify-between">
+        <li><a href="#">Collection</a></li>
+        <li>+</li>
+        <li><a href="#">Collaboration</a></li>
+        <li>+</li>
+        <li><a href="#">Gallery</a></li>
+        <li>+</li>
+        <li><a href="#">Our Story</a></li>
+        <li>+</li>
+        <li><a href="#">Press</a></li>
+      </ul>
+    </nav>
+    <section class="pb-15">
+      <p class="text-center">
+        Copyright © 2020 Art + Loom. All Rights Reserved.
+      </p>
+    </section>
+  </footer>
 </template>
+
+<style scoped>
+.separator {
+  border-top: 0.5px solid #000000;
+  width: 50px;
+}
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex flex-row items-center justify-between pt-11 px-15">
     <div>
-      <img src="/images/logo.svg" alt="" />
+      <img src="/images/logo.svg" alt="Art and loom" />
     </div>
     <nav class="w-2/3">
       <ul class="flex flex-row list-none m-0 p-0 justify-between">
