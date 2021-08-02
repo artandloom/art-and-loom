767e14c7e96ca0ae1edbbb8141af6ac4031efaa8
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-08-02 21:13:26 +0200

    Add slider demo

---
+++ pages/slider.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="swipers">
+    <swiper
+      class="swiper"
+      ref="swipers"
+      v-for="i in 2"
+      :key="i"
+      :options="swiperOption"
+    >
+      <swiper-slide v-for="i in 9" :key="i">Slide {{ i + 1 }}</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+
+export default {
+  name: "swiper-example-multiple-swipers",
+  title: "Multiple swipers",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      swiperOption: {
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      },
+    };
+  },
+  mounted() {
+    // console.log('swipers', this.$refs.swipers)
+  },
+};
+</script>
