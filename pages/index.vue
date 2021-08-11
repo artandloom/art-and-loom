11ece1500ca96ad4a1bd6359c08caa233c3b70ce
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-08-11 21:58:50 +0200

    Add collections button

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,91 @@
 <template>
-  <main>
+  <div>
     <Header />
-    <section>
-      <swiper class="swiper" :options="swiperOptionsMain">
-        <swiper-slide
-          class="flex justify-center items-center"
-          :key="banner.id"
-          v-for="banner in data.banners"
-        >
-          <p
-            class="absolute text-7xl uppercase"
-            :class="banner.color === 'dark' ? 'text-black' : 'text-white'"
+    <main class="px-15">
+      <section>
+        <swiper class="swiper" :options="swiperOptionsMain">
+          <swiper-slide
+            class="flex justify-center items-center"
+            :key="banner.id"
+            v-for="banner in data.banners"
           >
-            {{ banner.name }}
-          </p>
-          <img class="m-auto" :src="banner.image.data.full_url" />
-        </swiper-slide>
-      </swiper>
-    </section>
-    <section class="px-15 mb-30">
-      <h2 class="flex flex-row items-center uppercase">
-        Collections.
-        <span class="flex ml-6 separator"></span>
-      </h2>
-      <img
-        class="w-4/5"
-        :src="data.collection_cover.data.full_url"
-        alt="Collections"
-      />
-    </section>
-    <section class="px-15 mb-35 text-right">
-      <swiper class="swiper mb-4" :options="swiperOptionsItem">
-        <swiper-slide
-          class="flex justify-center items-center item"
-          :key="collection.id"
-          v-for="collection in collections"
-        >
-          <NuxtLink
-            :title="collection.name"
-            :to="'/collections/' + collection.slug + '-iid' + collection.id"
+            <p
+              class="absolute text-7xl uppercase"
+              :class="banner.color === 'dark' ? 'text-black' : 'text-white'"
+            >
+              {{ banner.name }}
+            </p>
+            <img class="m-auto" :src="banner.image.data.full_url" />
+          </swiper-slide>
+        </swiper>
+      </section>
+      <section class="mb-30">
+        <h2 class="flex flex-row items-center uppercase">
+          Collections.
+          <span class="flex ml-6 separator"></span>
+        </h2>
+        <img
+          class="w-4/5"
+          :src="data.collection_cover.data.full_url"
+          alt="Collections"
+        />
+      </section>
+      <section class="-mx-15 mb-35 text-right">
+        <swiper class="swiper mb-4" :options="swiperOptionsItem">
+          <swiper-slide
+            class="flex justify-center items-center item"
+            :key="collection.id"
+            v-for="collection in collections"
           >
-            <Item
-              :name="collection.name"
-              :image="collection.picture.data.full_url"
-            />
-          </NuxtLink>
-        </swiper-slide>
-      </swiper>
-      <NuxtLink class="uppercase" title="Collections" to="/collections"
-        >See all Collections.</NuxtLink
-      >
-    </section>
-    <section class="flex flex-col items-end px-15 mb-30">
-      <h2 class="flex flex-row items-center uppercase">
-        <span class="flex mr-6 separator"></span>
-        Collaborations.
-      </h2>
-      <img
-        class="w-4/5"
-        :src="data.collaboration_cover.data.full_url"
-        alt="Collaborations"
-      />
-    </section>
-    <section class="mb-35">
-      <swiper class="swiper" :options="swiperOptionsItem">
-        <swiper-slide
-          class="flex justify-center items-center item"
-          :key="collection.id"
-          v-for="collection in collections"
-        >
-          <NuxtLink
-            :to="'/collections/' + collection.slug + '-iid' + collection.id"
+            <NuxtLink
+              :title="collection.name"
+              :to="'/collections/' + collection.slug + '-iid' + collection.id"
+            >
+              <Item
+                :name="collection.name"
+                :image="collection.picture.data.full_url"
+              />
+            </NuxtLink>
+          </swiper-slide>
+        </swiper>
+        <div class="px-15">
+          <NuxtLink class="uppercase" title="Collections" to="/collections"
+            >See all Collections.</NuxtLink
           >
-            <Item
-              :name="collection.name"
-              :image="collection.picture.data.full_url"
-            />
-          </NuxtLink>
-        </swiper-slide>
-      </swiper>
-    </section>
+        </div>
+      </section>
+      <section class="flex flex-col items-end mb-30">
+        <h2 class="flex flex-row items-center uppercase">
+          <span class="flex mr-6 separator"></span>
+          Collaborations.
+        </h2>
+        <img
+          class="w-4/5"
+          :src="data.collaboration_cover.data.full_url"
+          alt="Collaborations"
+        />
+      </section>
+      <section class="mb-35">
+        <swiper class="swiper" :options="swiperOptionsItem">
+          <swiper-slide
+            class="flex justify-center items-center item"
+            :key="collection.id"
+            v-for="collection in collections"
+          >
+            <NuxtLink
+              :to="'/collections/' + collection.slug + '-iid' + collection.id"
+            >
+              <Item
+                :name="collection.name"
+                :image="collection.picture.data.full_url"
+              />
+            </NuxtLink>
+          </swiper-slide>
+        </swiper>
+      </section>
+    </main>
     <Footer />
-  </main>
+  </div>
 </template>
 <script>
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
@@ -97,11 +101,14 @@ export default {
       },
       swiperOptionsItem: {
         slidesPerView: "auto",
-        spaceBetween: 8,
         preventClicks: false,
         preventClicksPropagation: false,
         resistance: true,
         resistanceRatio: 0.65,
+        spaceBetween: 40,
+        slidesOffsetAfter: 20,
+        slidesOffsetBefore: 60,
+        slidesPerView: 4,
       },
     };
   },
@@ -136,7 +143,7 @@ h2 {
   border-top: 0.5px solid #000000;
   width: 200px;
 }
-.item {
+/* .item {
   width: 25% !important;
-}
+} */
 </style>
