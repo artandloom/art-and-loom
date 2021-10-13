7c81929f550808564718f6c055956f2f76d5d0ef
Gabriel Fiocchi <gab@thinkapp.studio>
2021-10-13 13:38:29 +0200

    Update index.vue

--- pages/press/index.vue
+++ pages/press/index.vue
@@ -21,7 +21,7 @@
         </div>
       </div>
     </section>
-    <section class="flex flex-row pt-15 -mx-4">
+    <section class="flex flex-row flex-wrap pt-15 -mx-4">
       <div class="w-1/3 m-4 press-item" v-for="item in data" :key="item.id">
         <NuxtLink :to="'/press/' + item.slug">
           <!-- TODO: Add image component -->
