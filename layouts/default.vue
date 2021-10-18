48df341e96e68fde5b8846b2216d4acb9bbe651d
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-10-18 22:35:43 +0200

    Remove unnecesary bottom space

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="default">
     <Header />
     <Nuxt
-      class="px-5 pb-15 md:px-10 md:pb-20 lg:px-15 lg:pb-30 overflow-hidden"
+      class="px-5 pb-15 md:px-10 md:pb-20 lg:px-15 overflow-hidden"
     />
     <SubscribeNewsletter />
     <Footer />
