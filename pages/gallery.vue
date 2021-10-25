80b98bbc665c3d80914a9512cf95e22d2fee62cb
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2021-10-25 16:37:24 +0200

    Add lightbox

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,5 +1,12 @@
 <template>
   <main>
+    <CoolLightBox
+      :items="images"
+      :index="sliderIndex"
+      @close="sliderIndex = null"
+    >
+    </CoolLightBox>
+
     <section>
       <div class="relative -mx-15">
         <div
@@ -14,7 +21,18 @@
             justify-end
           "
         >
-          <h2 class="flex flex-row items-center uppercase text-3xl md:text-5xl lg:text-7xl z-10 px-15">
+          <h2
+            class="
+              flex flex-row
+              items-center
+              uppercase
+              text-3xl
+              md:text-5xl
+              lg:text-7xl
+              z-10
+              px-15
+            "
+          >
             <span class="flex mr-6 separator"></span>
             Gallery
           </h2>
@@ -22,7 +40,12 @@
       </div>
     </section>
     <section class="flex flex-row flex-wrap pt-15 -mx-4">
-      <div class="w-full md:w-1/2 lg:w-1/3 p-4 press-item" v-for="item in data" :key="item.id">
+      <div
+        class="w-full md:w-1/2 lg:w-1/3 p-4 press-item"
+        v-for="(item, index) in data"
+        :key="item.id"
+        @click="updateIndexImage(index)"
+      >
         <!-- TODO: Add image component -->
         <img
           class="w-full"
@@ -31,17 +54,14 @@
           :srcset="
             baseUrl +
             item.image.private_hash +
-          
             imageSrc.xsmall +
             ' 200w,' +
             baseUrl +
             item.image.private_hash +
-          
             imageSrc.small +
             ' 458w,' +
             baseUrl +
             item.image.private_hash +
-          
             imageSrc.medium +
             ' 500w'
           "
@@ -53,13 +73,26 @@
 </template>
 
 <script>
+import CoolLightBox from "vue-cool-lightbox";
+import "vue-cool-lightbox/dist/vue-cool-lightbox.min.css";
+
 export default {
+  components: {
+    CoolLightBox,
+  },
   async asyncData({ $axios }) {
     const { data } = await $axios.$get(
-      process.env.baseUrl + "/gallery?fields=*.*.*"
+      process.env.baseUrl +
+        "/gallery?fields=id,name,image.private_hash,image.data"
     );
 
-    return { data };
+    const images = [];
+
+    data.map((item) => {
+      images.push(item.image.data.full_url);
+    });
+
+    return { data, images };
   },
   data: () => ({
     baseUrl: "https://admin.artandloom.com/art-and-loom/assets/",
@@ -68,7 +101,13 @@ export default {
       small: "?w=458&h=458&q=72&f=crop",
       medium: "?w=500&h=500&q=72&f=crop",
     },
+    sliderIndex: null,
   }),
+  methods: {
+    updateIndexImage(index) {
+      this.sliderIndex = index;
+    },
+  },
 };
 </script>
 <style scoped>
