31958e785aa0614b55edb430084299f9d1293aa3
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2022-08-29 07:24:24 -0300

    Update api config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-import express from 'express';
+import * as express from 'express';
 import bodyParser from 'body-parser';
-// const express = require('express');
 
 // Create express instance
 const app = express();
